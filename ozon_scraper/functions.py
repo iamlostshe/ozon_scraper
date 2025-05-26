739014ec5c3cdc68e31375ce6f352efb9cc41522
ivan <vanamelcikov7275@gmail.com>
2025-05-26 20:03:32 +0400

    Linter autofix

--- ozon_scraper/functions.py
+++ ozon_scraper/functions.py
@@ -1,10 +1,11 @@
 import time as tm
+
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 
 
 def page_down(driver):
-    driver.execute_script('''
+    driver.execute_script("""
                             const scrollStep = 200; // Размер шага прокрутки (в пикселях)
                             const scrollInterval = 100; // Интервал между шагами (в миллисекундах)
 
@@ -18,12 +19,12 @@ def page_down(driver):
                                     clearInterval(interval);
                                 }
                             }, scrollInterval);
-                        ''')
+                        """)
 
 
-def collect_product_info(driver, url=''):
+def collect_product_info(driver, url=""):
 
-    driver.switch_to.new_window('tab')
+    driver.switch_to.new_window("tab")
 
     tm.sleep(3)
     driver.get(url=url)
@@ -31,19 +32,19 @@ def collect_product_info(driver, url=''):
 
     # product_id
     product_id = driver.find_element(
-        By.XPATH, '//div[contains(text(), "Артикул: ")]'
-    ).text.split('Артикул: ')[1]
+        By.XPATH, '//div[contains(text(), "Артикул: ")]',
+    ).text.split("Артикул: ")[1]
 
     # print(product_id)
 
     page_source = str(driver.page_source)
-    soup = BeautifulSoup(page_source, 'lxml')
+    soup = BeautifulSoup(page_source, "lxml")
 
-    with open(f'product_{product_id}.html', 'w') as file:
+    with open(f"product_{product_id}.html", "w") as file:
         file.write(page_source)
 
-    product_name = soup.find('div', attrs={"data-widget": 'webProductHeading'}).find(
-        'h1').text.strip().replace('\t', '').replace('\n', ' ')
+    product_name = soup.find("div", attrs={"data-widget": "webProductHeading"}).find(
+        "h1").text.strip().replace("\t", "").replace("\n", " ")
 
     # product_id
     # try:
@@ -55,11 +56,11 @@ def collect_product_info(driver, url=''):
     # product statistic
     try:
         product_statistic = soup.find(
-            'div', attrs={"data-widget": 'webSingleProductScore'}).text.strip()
+            "div", attrs={"data-widget": "webSingleProductScore"}).text.strip()
 
         if " • " in product_statistic:
-            product_stars = product_statistic.split(' • ')[0].strip()
-            product_reviews = product_statistic.split(' • ')[1].strip()
+            product_stars = product_statistic.split(" • ")[0].strip()
+            product_reviews = product_statistic.split(" • ")[1].strip()
         else:
             product_statistic = product_statistic
     except:
@@ -70,17 +71,17 @@ def collect_product_info(driver, url=''):
     # product price
     try:
         ozon_card_price_element = soup.find(
-            'span', string="c Ozon Картой").parent.find('div').find('span')
+            "span", string="c Ozon Картой").parent.find("div").find("span")
         product_ozon_card_price = ozon_card_price_element.text.strip(
-        ) if ozon_card_price_element else ''
+        ) if ozon_card_price_element else ""
 
         price_element = soup.find(
-            'span', string="без Ozon Карты").parent.parent.find('div').findAll('span')
+            "span", string="без Ozon Карты").parent.parent.find("div").findAll("span")
 
         product_discount_price = price_element[0].text.strip(
-        ) if price_element[0] else ''
+        ) if price_element[0] else ""
         product_base_price = price_element[1].text.strip(
-        ) if price_element[1] is not None else ''
+        ) if price_element[1] is not None else ""
     except:
         product_ozon_card_price = None
         product_discount_price = None
@@ -89,24 +90,24 @@ def collect_product_info(driver, url=''):
     # product price
     try:
         ozon_card_price_element = soup.find(
-            'span', string="c Ozon Картой").parent.find('div').find('span')
+            "span", string="c Ozon Картой").parent.find("div").find("span")
     except AttributeError:
         card_price_div = soup.find(
-            'div', attrs={"data-widget": "webPrice"}).findAll('span')
+            "div", attrs={"data-widget": "webPrice"}).findAll("span")
 
         product_base_price = card_price_div[0].text.strip()
         product_discount_price = card_price_div[1].text.strip()
 
     product_data = (
         {
-            'product_id': product_id,
-            'product_name': product_name,
-            'product_ozon_card_price': product_ozon_card_price,
-            'product_discount_price': product_discount_price,
-            'product_base_price': product_base_price,
-            'product_statistic': product_statistic,
-            'product_stars': product_stars,
-            'product_reviews': product_reviews,
+            "product_id": product_id,
+            "product_name": product_name,
+            "product_ozon_card_price": product_ozon_card_price,
+            "product_discount_price": product_discount_price,
+            "product_base_price": product_base_price,
+            "product_statistic": product_statistic,
+            "product_stars": product_stars,
+            "product_reviews": product_reviews,
         }
     )
 
--- ozon_scraper/main.py
+++ ozon_scraper/main.py
@@ -1,22 +1,20 @@
 import json
 import time
-import undetected_chromedriver as uc 
-from bs4 import BeautifulSoup
+
+import undetected_chromedriver as uc
+from functions import collect_product_info
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.support.ui import WebDriverWait
-from functions import page_down, collect_product_info
 
 
-def get_products_links(item_name='наушники hyperx'):
+def get_products_links(item_name="наушники hyperx"):
     driver = uc.Chrome()
     driver.implicitly_wait(5)
 
-    driver.get(url='https://ozon.ru')
+    driver.get(url="https://ozon.ru")
     time.sleep(2)
 
-    find_input = driver.find_element(By.NAME, 'text')
+    find_input = driver.find_element(By.NAME, "text")
     find_input.clear()
     find_input.send_keys(item_name)
     time.sleep(2)
@@ -24,7 +22,7 @@ def get_products_links(item_name='наушники hyperx'):
     find_input.send_keys(Keys.ENTER)
     time.sleep(2)
 
-    current_url = f'{driver.current_url}&sorting=rating'
+    current_url = f"{driver.current_url}&sorting=rating"
     driver.get(url=current_url)
     time.sleep(2)
 
@@ -32,19 +30,19 @@ def get_products_links(item_name='наушники hyperx'):
     time.sleep(2)
 
     try:
-        find_links = driver.find_elements(By.CLASS_NAME, 'tile-hover-target')
+        find_links = driver.find_elements(By.CLASS_NAME, "tile-hover-target")
         products_urls = list(set([f'{link.get_attribute("href")}' for link in find_links]))
 
-        print('[+] Ссылки на товары собраны!')
+        print("[+] Ссылки на товары собраны!")
     except:
-        print('[!] Что-то сломалось при сборе ссылок на товары!')
+        print("[!] Что-то сломалось при сборе ссылок на товары!")
 
     products_urls_dict = {}
 
     for k, v in enumerate(products_urls):
         products_urls_dict.update({k: v})
 
-    with open('products_urls_dict.json', 'w', encoding='utf-8') as file:
+    with open("products_urls_dict.json", "w", encoding="utf-8") as file:
         json.dump(products_urls_dict, file, indent=4, ensure_ascii=False)
 
     time.sleep(2)
@@ -57,7 +55,7 @@ def get_products_links(item_name='наушники hyperx'):
         time.sleep(2)
         products_data.append(data)
 
-    with open('PRODUCTS_DATA.json', 'w', encoding='utf-8') as file:
+    with open("PRODUCTS_DATA.json", "w", encoding="utf-8") as file:
         json.dump(products_data, file, indent=4, ensure_ascii=False)
 
     driver.close()
@@ -65,6 +63,6 @@ def get_products_links(item_name='наушники hyperx'):
 
 
 def main():
-    print('[INFO] Сбор данных начался. Пожалуйста ожидайте...')
-    get_products_links(item_name='наушники hyperx')
-    print('[INFO] Работа выполнена успешно!')
+    print("[INFO] Сбор данных начался. Пожалуйста ожидайте...")
+    get_products_links(item_name="наушники hyperx")
+    print("[INFO] Работа выполнена успешно!")
